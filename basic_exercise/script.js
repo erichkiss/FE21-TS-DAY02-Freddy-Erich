6df24d758a0a665b295a329d484ab072949db1f8
Erich Kiss <erich.kiss2@gmail.com>
2021-03-18 11:39:11 +0100

    exercise 1 final

--- basic_exercise/script.js
+++ basic_exercise/script.js
@@ -1,21 +1,7 @@
 // Create a class constructor named Person that should hold information about name, age,  jobTitle and have a function that will return a string “Hello there, 
 // My name is (name) and I am (age) years old, and I am a (jobTitle)”
-
-interface PersonTemplate {
-    fName : string;
-    lName : string;
-    age : number;
-    jobTitle : string;
-    employed : boolean;
-}
-
-class Person implements PersonTemplate {
-    fName;
-    lName;
-    age;
-    jobTitle;
-    employed;
-    constructor (fN : string, lN : string, ag : number, jobT :string, empl : boolean) {
+var Person = /** @class */ (function () {
+    function Person(fN, lN, ag, jobT, empl) {
         this.fName = fN;
         this.lName = lN;
         this.age = ag;
@@ -23,14 +9,12 @@ class Person implements PersonTemplate {
         this.employed = empl;
         console.log(this);
     }
-    showMessage () {
-        return `My name is ${this.fName} ${this.lName} and I am ${this.age} old, and I am a ${this.jobTitle}`;
-    }
-}
-
+    Person.prototype.showMessage = function () {
+        return "My name is " + this.fName + " " + this.lName + " and I am " + this.age + " old, and I am a " + this.jobTitle;
+    };
+    return Person;
+}());
 var person1 = new Person("Erich", "Kiss", 39, "clown", false);
 var person2 = new Person("Freddy", "Delgado", 35, "extra-clown", true);
-
 console.log(person1.showMessage());
 console.log(person2.showMessage());
-
