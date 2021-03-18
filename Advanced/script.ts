54655ac2b3749c88a7a0139dda608eea3292a3e7
Erich Kiss <erich.kiss2@gmail.com>
2021-03-18 14:11:02 +0100

    master

--- Advanced/script.ts
+++ Advanced/script.ts
@@ -1,3 +1,38 @@
 class Vehicles {
+    brand : string;
+    constructionYear : number;
+    type : string;
+    doors : number;
+    tuev : boolean;
 
+    constructor (br : string, co : number, ty : string, doo : number, tu : boolean) {
+        this.brand = br;
+        this.constructionYear = co;
+        this.type = ty;
+        this.doors = doo;
+        this.tuev = tu;
+    }
+
+    showMessageCar() {
+        // return `${this.brand} and ${this.constructionYear} and ${this.type} and ${this.doors} and ${this.tuev}.<br>`;
+    }
+}
+
+class Motorbike extends Vehicles {
+    luggageRack;
+    sideCar;
+
+    constructor (br : string, co : number, ty : string, doo : number, tu : boolean, lugg : string, side : boolean) {
+        super(br, co, ty, doo, tu);
+        this.luggageRack = lugg;
+        this.sideCar = side;
+    }
+
+    showMessage() {
+        // return super.showMessageCar() + " and " + this.luggageRack + " and " + this.sideCar + "<br>";
+    }
+}
+
+class Truck extends Vehicles {
+    
 }
